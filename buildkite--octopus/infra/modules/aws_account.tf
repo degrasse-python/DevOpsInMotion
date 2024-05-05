e07ed7debeaf1fb9556bebd36832208d3caf8ce6
Deon Saunders <51129091+degrasse-python@users.noreply.github.com>
2024-05-05 19:17:32 -0400

    Add EKS cluster

--- buildkite--octopus/infra/modules/aws_account.tf
+++ buildkite--octopus/infra/modules/aws_account.tf
@@ -10,6 +10,11 @@ provider "aws" {
   secret_key = var.AWS_SECRET_ACCESS_KEY
 }
 
+/*
+
+Data
+
+*/
 
 data "aws_vpc" "default" {
   default = true
@@ -26,6 +31,12 @@ data "aws_subnet" "subnet_set" {
   for_each = toset(data.aws_subnets.default.ids)
   id       = each.value
 }
+
+/*
+
+Security 
+
+*/
 
 # Create a security group allowing SSH access from anywhere
 resource "aws_security_group" "buildkite_sg" {
@@ -59,6 +70,67 @@ resource "aws_key_pair" "buildkite_ssh_key" {
   public_key = tls_private_key.buildkite_ssh_key.public_key_openssh
 }
 
+# Create an IAM role for the EKS cluster
+resource "aws_iam_role" "eks_admin_role" {
+  name               = "eks-cluster-role"
+  assume_role_policy = jsonencode({
+    Version   = "2012-10-17",
+    Statement = [
+      {
+        Effect    = "Allow",
+        Principal = {
+          Service = "eks.amazonaws.com"
+        },
+        Action    = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# Attach IAM policies to the role
+resource "aws_iam_policy_attachment" "eks_policy_attachment" {
+  name       = "eks-cluster-policy-attachment"
+  roles      = [aws_iam_role.eks_admin_role.name]
+
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"  # Provides permissions to create and manage EKS clusters
+}
+
+resource "aws_iam_policy_attachment" "eks_node_policy_attachment" {
+  name       = "eks-node-policy-attachment"
+  roles      = [aws_iam_role.eks_admin_role.name]
+
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"  # Provides permissions to attach IAM roles to worker nodes
+}
+
+resource "aws_iam_policy_attachment" "eks_cni_policy_attachment" {
+  name       = "eks-cni-policy-attachment"
+  roles      = [aws_iam_role.eks_admin_role.name]
+
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"  # Provides permissions for the Amazon VPC CNI plugin used by EKS clusters
+}
+
+resource "aws_iam_policy_attachment" "eks_service_policy_attachment" {
+  name       = "eks-service-policy-attachment"
+  roles      = [aws_iam_role.eks_admin_role.name]
+
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"  # Provides permissions for Kubernetes service discovery via AWS CloudMap
+}
+
+resource "aws_iam_policy_attachment" "eks_fargate_policy_attachment" {
+  name       = "eks-fargate-policy-attachment"
+  roles      = [aws_iam_role.eks_admin_role.name]
+
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy"  # Provides permissions for Fargate pod execution role
+}
+
+
+
+/*
+
+EC2
+
+*/
+
 # Create an EC2 instance for the Buildkite agent
 resource "aws_instance" "buildkite_instance" {
   ami                    = "ami-0c55b159cbfafe1f0"  # Ubuntu 20.04 LTS AMI
@@ -89,7 +161,33 @@ resource "aws_instance" "buildkite_instance" {
 
 }
 
-# Output the public IP address of the instance
-output "buildkite_instance_public_ip" {
-  value = aws_instance.buildkite_instance.public_ip
+/*
+
+EKS
+
+*/
+
+
+resource "aws_eks_cluster" "prod" {
+  name     = "prod"
+  role_arn = aws_iam_role.eks_admin_role.arn
+
+  vpc_config {
+    subnet_ids = [data.aws_subnets.default.ids[0], data.aws_subnets.default.ids[1]]
+  
+  }
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
+  # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cni_policy_attachment,
+    aws_iam_role_policy_attachment.eks_fargate_policy_attachment,
+    aws_iam_role_policy_attachment.eks_node_policy_attachment,
+    aws_iam_role_policy_attachment.eks_policy_attachment,
+    aws_iam_role_policy_attachment.eks_service_policy_attachment,
+  ]
+}
+
+output "endpoint" {
+  value = aws_eks_cluster.prod.endpoint
 }
