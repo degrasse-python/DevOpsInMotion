02ccfa8971e55d41164bba6c1ef638ec95f19b83
Deon Saunders <51129091+degrasse-python@users.noreply.github.com>
2024-05-05 18:37:44 -0400

    Update aws_account.tf
    removed deprecated stuff

--- buildkite--octopus/infra/modules/aws_account.tf
+++ buildkite--octopus/infra/modules/aws_account.tf
@@ -15,14 +15,9 @@ data "aws_vpc" "default" {
   default = true
 }
 
-
-data "aws_subnet_ids" "default" {
-  vpc_id = data.aws_vpc.default.id
-}
-
 data "aws_subnets" "default" {
   filter {
-    name   = "vpc-default"
+    name   = "vpc-id"
     values = [data.aws_vpc.default.id]
   }
 }
