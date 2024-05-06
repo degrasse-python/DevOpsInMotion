7a170e74592c17cf16c5d8e72dfde594a5f63944
Deon Saunders <51129091+degrasse-python@users.noreply.github.com>
2024-05-06 18:47:14 -0400

    changed playbook

--- buildkite--octopus/infra/modules/aws_account.tf
+++ buildkite--octopus/infra/modules/aws_account.tf
@@ -154,7 +154,7 @@ resource "aws_instance" "buildkite_instance" {
   
   provisioner "local-exec" {
     # √buildkite--octopus/infra/ansible/roles/buildkite/tasks/install.yaml
-    command = "ansible-playbook -i ${aws_instance.buildkite_instance.public_ip}, --private-key ${tls_private_key.buildkite_ssh_key.private_key_pem} ../ansible/roles/buildkite/tasks/install.yaml"
+    command = "ansible-playbook -i ${aws_instance.buildkite_instance.public_ip}, --private-key ${tls_private_key.buildkite_ssh_key.private_key_pem} ../ansible/playbook.yaml"
   }
 
 }
