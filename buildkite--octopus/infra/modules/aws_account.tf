b22268e68e13c878f2b1963733a789b2ccf706c1
Deon Saunders <51129091+degrasse-python@users.noreply.github.com>
2024-05-10 11:42:57 -0400

    added user

--- buildkite--octopus/infra/modules/aws_account.tf
+++ buildkite--octopus/infra/modules/aws_account.tf
@@ -1,5 +1,5 @@
 locals {
-  ssh_user = "root"
+  ssh_user = "ubuntu"
   key_name = "buildkite_ssh_key"
   # private_key_path = "~/githubRepos/.ssh/devops.pem"
 }
@@ -157,7 +157,7 @@ resource "aws_instance" "buildkite_instance" {
     # √buildkite--octopus/infra/ansible/roles/buildkite/tasks/install.yaml
     working_dir = path.cwd
     # working dir is buildkite--octopus/infra/
-    command = "ansible-playbook -i ${aws_instance.buildkite_instance.public_ip}, --private-key ${tls_private_key.buildkite_ssh_key.private_key_pem} ansible/playbook.yaml"
+    command = "ansible-playbook -u ubuntu -i ${aws_instance.buildkite_instance.public_ip}, --private-key ${tls_private_key.buildkite_ssh_key.private_key_pem} ansible/playbook.yaml"
   }
 
 }
